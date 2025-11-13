2a464bef839b5e2b4a5064944d8b0c01640b0b6b
Shaul Bar-Lev <shaulbarlev1@gmail.com>
2025-11-13 14:32:57 +0200

    preprocess

--- Anagram.java
+++ Anagram.java
@@ -1,29 +1,32 @@
 /** Functions for checking if a given string is an anagram. */
 public class Anagram {
 	public static void main(String args[]) {
-		// Tests the isAnagram function.
-		System.out.println(isAnagram("silent","listen"));  // true
-		System.out.println(isAnagram("William Shakespeare","I am a weakish speller")); // true
-		System.out.println(isAnagram("Madam Curie","Radium came")); // true
-		System.out.println(isAnagram("Tom Marvolo Riddle","I am Lord Voldemort")); // true
 
-		// Tests the preProcess function.
-		System.out.println(preProcess("What? No way!!!"));
+	System.out.println(preProcess("HEEE!!!!! ok."));
+
+	// 	// Tests the isAnagram function.
+	// 	System.out.println(isAnagram("silent","listen"));  // true
+	// 	System.out.println(isAnagram("William Shakespeare","I am a weakish speller")); // true
+	// 	System.out.println(isAnagram("Madam Curie","Radium came")); // true
+	// 	System.out.println(isAnagram("Tom Marvolo Riddle","I am Lord Voldemort")); // true
+
+	// 	// Tests the preProcess function.
+	// 	System.out.println(preProcess("What? No way!!!"));
 		
-		// Tests the randomAnagram function.
-		System.out.println("silent and " + randomAnagram("silent") + " are anagrams.");
+	// 	// Tests the randomAnagram function.
+	// 	System.out.println("silent and " + randomAnagram("silent") + " are anagrams.");
 		
-		// Performs a stress test of randomAnagram 
-		String str = "1234567";
-		Boolean pass = true;
-		//// 10 can be changed to much larger values, like 1000
-		for (int i = 0; i < 10; i++) {
-			String randomAnagram = randomAnagram(str);
-			System.out.println(randomAnagram);
-			pass = pass && isAnagram(str, randomAnagram);
-			if (!pass) break;
-		}
-		System.out.println(pass ? "test passed" : "test Failed");
+	// 	// Performs a stress test of randomAnagram 
+	// 	String str = "1234567";
+	// 	Boolean pass = true;
+	// 	//// 10 can be changed to much larger values, like 1000
+	// 	for (int i = 0; i < 10; i++) {
+	// 		String randomAnagram = randomAnagram(str);
+	// 		System.out.println(randomAnagram);
+	// 		pass = pass && isAnagram(str, randomAnagram);
+	// 		if (!pass) break;
+	// 	}
+	// 	System.out.println(pass ? "test passed" : "test Failed");
 	}  
 
 	// Returns true if the two given strings are anagrams, false otherwise.
@@ -37,7 +40,18 @@ public class Anagram {
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
 		// Replace the following statement with your code
-		return "";
+		String lowerCase = str.toLowerCase();
+		String out = "";
+		String badChars = " _?!.,";
+
+		for (int i = 0; i < lowerCase.length(); i++) {
+			char c = lowerCase.charAt(i);
+			if (badChars.indexOf(c) == -1 ) {
+				out = out + c;
+			}
+		}
+
+		return out;
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
